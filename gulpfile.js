d719a8d5f4faa96a133922637372ba43e4208d56
Kapatych <onetruestory@ya.ru>
2020-03-12 17:55:39 +0300

    Add and customization gulp 4

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+
+function style() {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([
+      autoprefixer({overrideBrowserslist: ["last 2 versions"]})
+    ]))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch('./sass/**/*.scss', style);
+  gulp.watch("./*.html").on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
