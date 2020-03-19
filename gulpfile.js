6b8c219a6a403890246f6085363482e437dc456b
Kapatych <onetruestory@ya.ru>
2020-03-19 22:08:31 +0300

    Add htmlmin, optimize fonts in html with preload and in css with font-display

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 const gulp = require('gulp');
+const babel = require("gulp-babel");
 const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
-const plumber = require("gulp-plumber");
 const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
-const del = require("del");
-const minify = require("gulp-csso");
+const autoPrefixer = require("autoprefixer");
 const rename = require("gulp-rename");
-const imagemin = require('gulp-imagemin');
-const uglify = require("gulp-uglify");
-const babel = require('gulp-babel');
+const del = require("del");
+const plumber = require("gulp-plumber");
+const htmlMinify = require("gulp-htmlmin");
+const cssMinify = require("gulp-csso");
+const jsMinify = require("gulp-uglify");
+const imageMinify = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
 
 function copy() {
   return gulp.src([
@@ -29,10 +30,10 @@ function style() {
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([
-      autoprefixer({overrideBrowserslist: ["last 2 versions"]})
+      autoPrefixer({overrideBrowserslist: ["last 2 versions"]})
     ]))
     .pipe(gulp.dest('build/css'))
-    .pipe(minify())
+    .pipe(cssMinify())
     .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest('build/css'))
     .pipe(browserSync.stream());
@@ -42,24 +43,25 @@ function js() {
   return gulp.src("src/js/script.js")
     .pipe(gulp.dest("build/js"))
     .pipe(babel({presets: ['@babel/env']}))
-    .pipe(uglify())
+    .pipe(jsMinify())
     .pipe(rename({ extname: '.min.js' }))
     .pipe(gulp.dest("build/js"))
 }
 
 function image() {
   return gulp.src('src/img/**')
-    .pipe(imagemin([
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.mozjpeg({quality: 75, progressive: true}),
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.svgo()
+    .pipe(imageMinify([
+      imageMinify.gifsicle({interlaced: true}),
+      imageMinify.mozjpeg({quality: 75, progressive: true}),
+      imageMinify.optipng({optimizationLevel: 3}),
+      imageMinify.svgo()
     ]))
     .pipe(gulp.dest('build/img'))
 }
 
 function html() {
   return gulp.src("src/*.html")
+    .pipe(htmlMinify({ collapseWhitespace: true }))
     .pipe(gulp.dest("build"))
 }
 
@@ -85,5 +87,5 @@ exports.serve = serve;
 exports.build = gulp.series(
   clean,
   copy,
-  gulp.parallel(style, js, image, html)
+  gulp.parallel(html, style, js, image)
 );
